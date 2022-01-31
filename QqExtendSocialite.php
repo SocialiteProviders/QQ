00bf77170e73be69eb6fe661def3aa1ae5b7035d
Omar Vega Ramos <ovruni@riseup.net>
2022-01-30 20:29:41 -0500

    Add Mediawiki Provider (#786)
    Co-authored-by: Omar Vega Ramos <ovruni@gnu.org.pe>

---
+++ QqExtendSocialite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SocialiteProviders\QQ;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class QqExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('qq', Provider::class);
+    }
+}
